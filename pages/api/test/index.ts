5b38714cf9afaa6512b40e27084de6b160c15b76
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2023-05-02 21:50:48 +0200

    api test route fix

--- pages/api/test/index.ts
+++ pages/api/test/index.ts
@@ -5,6 +5,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.json({ message: "Bien reçu, Bien reçu" });
   } else {
     res.status(405).json({ message: "Method not allowed" });
